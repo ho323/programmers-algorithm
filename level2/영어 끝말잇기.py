49f4119332bacfd65f6c5027a36126b64a14c783
Hoseong Kim <86637300+ho323@users.noreply.github.com>
2023-04-19 02:27:16 +0900

    Update 영어 끝말잇기.py

--- level2/영어 끝말잇기.py
+++ level2/영어 끝말잇기.py
@@ -6,10 +6,7 @@ def solution(n, words):
     for i in range(1, w):
         if (words[i] in arr) or (words[i-1][-1] != words[i][0]):
             x=(i%n)+1
-            # y=(i//n)+1
-            y = z
+            y=(i//n)+1
             break
         arr.append(words[i])
-        if (i+1) % n== 0:
-            z+=1
     return [x,y]
