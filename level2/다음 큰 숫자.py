726cc88a83cca2a324197b496849d3160ad99b28
Hoseong Kim <86637300+ho323@users.noreply.github.com>
2023-04-19 03:40:36 +0900

    Update 다음 큰 숫자.py

--- level2/다음 큰 숫자.py
+++ level2/다음 큰 숫자.py
@@ -1,9 +1,9 @@
 def solution(n):
     answer = 0
-    nc = list(bin(n)).count('1')
+    nc = bin(n).count('1')
     a=n+1
     while True:
-        if list(bin(a)).count('1') == nc:
+        if bin(a).count('1') == nc:
             break
         a+=1
     return a
